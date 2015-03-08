d2efcdbbcf76a8b57c72cd660f2e3ab49ff4897a
Henrique Leal <hm.leal@hotmail.com>
2015-03-07 21:14:06 -0300

    First commit.

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import json
+
+from flask import Flask, render_template, request, jsonify
+from lib import slugify, html_entities_to_ascii
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/suggests/')
+def suggests():
+    q = slugify(request.args.get('q'))
+
+    with open(os.path.join(BASE_DIR, 'static', 'suggests.json')) as file:
+        data = json.load(file)
+
+    suggests = {
+        'query': q,
+        'suggestions': [],
+        'suggestions_highlithed': []
+    }
+
+    has_header = False
+    for header in data['hightlights']:
+        for query in header['queries']:
+            query = html_entities_to_ascii(query)
+            if query.startswith(q):
+                has_header = True
+        if has_header:
+            suggests['has_header'] = True
+            suggests['header'] = header
+            break
+
+    for suggest in data['suggestions']:
+        suggest = html_entities_to_ascii(suggest)
+        if suggest.startswith(q):
+            new_suggest = {
+                'query': suggest,
+                'text': suggest.replace(q, '<b>%s</b>' % q)
+            }
+            suggests['suggestions'].append(new_suggest)
+
+    return jsonify(suggests)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/src/js/app.js
@@ -0,0 +1,40 @@
+$(function() {
+    var suggestions = $('#sugestoes'),
+        source = $('#suggests-template').html(),
+        template = Handlebars.compile(source);
+
+    suggestions.on('mouseover', '.sugestao', function() {
+        $this = $(this);
+        $this.addClass('sugestao-hover');
+    });
+    suggestions.on('mouseout', '.sugestao', function() {
+        $this = $(this);
+        $this.removeClass('sugestao-hover');
+    });
+    suggestions.on('click', '.sugestao', function(e) {
+        /* efetua o redirectionamento da página */
+        e.preventDefault();
+        url = $(this).data('url');
+        if (url !== "") {
+            window.location.href = url;
+        }
+    });
+    $('input[name=search]').on('focusout', function() {
+        /* esconde o pop-up de sugestões e limpa a busca antiga */
+        $('#sugestoes').hide();
+        $('#sugestoes ul li').remove();
+    });
+
+    $('input[name=search]').keyup(function() {
+        var input = $(this);
+        $.ajax({
+            url: '/suggests?q=' + input.val(),
+            dataType: 'json',
+            success: function(data) {
+                $('#sugestoes ul li').remove();
+                $('#sugestoes ul').append(template(data));
+                suggestions.show();
+            }
+        });
+    });
+});
