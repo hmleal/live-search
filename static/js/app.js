8640140bc60cd517dccaa92455b82b9a6c83a4a0
Henrique Leal <hm.leal@hotmail.com>
2015-03-07 22:46:18 -0300

    Atualiza layout para usar mais css ao invés de js.

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,12 @@
 $(function() {
-    var suggestions = $('#sugestoes'),
+    var suggestions = $('#suggestions'),
         source = $('#suggests-template').html(),
         template = Handlebars.compile(source);
 
-    suggestions.on('mouseover', '.sugestao', function() {
-        $this = $(this);
-        $this.addClass('sugestao-hover');
-    });
-    suggestions.on('mouseout', '.sugestao', function() {
-        $this = $(this);
-        $this.removeClass('sugestao-hover');
-    });
-    suggestions.on('click', '.sugestao', function(e) {
-        /* efetua o redirectionamento da página */
-        e.preventDefault();
-        url = $(this).data('url');
-        if (url !== "") {
-            window.location.href = url;
-        }
-    });
-
     $('input[name=search]').on('focusout', function() {
-        /* esconde o pop-up de sugestões e limpa a busca antiga
-        $('#sugestoes').hide();
-        $('#sugestoes ul li').remove();
-        */
+        /* esconde o pop-up de sugestões e limpa a busca antiga */
+        suggestions.hide();
+        $('#suggestions ul li').remove();
     });
 
     $('input[name=search]').keyup(function() {
@@ -33,8 +15,8 @@ $(function() {
             url: '/suggests?q=' + input.val(),
             dataType: 'json',
             success: function(data) {
-                $('#sugestoes ul li').remove();
-                $('#sugestoes ul').append(template(data));
+                $('#suggestions ul li').remove();
+                $('#suggestions ul').append(template(data));
                 suggestions.show();
             }
         });
