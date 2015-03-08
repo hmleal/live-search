60963629bc6d67ccd206321b73dd412e6326fac3
Henrique Leal <hm.leal@hotmail.com>
2015-03-07 23:03:50 -0300

    Adiciona verificação de porta para habilitar debug True.

--- app.py
+++ app.py
@@ -52,4 +52,7 @@ def suggests():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    if port == 5000:
+        app.debug = True
+    app.run()
